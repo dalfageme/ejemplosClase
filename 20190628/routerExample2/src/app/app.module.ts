764cf65b1998623f023d3916f7c93d8e0032f91d
David Alfageme <alfalfageme@gmail.com>
2019-06-28 11:51:14 +0200

    Router angular

---
+++ 20190628/routerExample2/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+import Routes from './app.router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page1Component,
+    Page2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(Routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 20190628/routerExample2/src/app/app.router.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+const routes: Routes = [
+    {
+        path: 'page1',
+        component: Page1Component
+    },
+    {
+        path: 'page2',
+        component: Page2Component
+    }
+];
+
+export default routes;
